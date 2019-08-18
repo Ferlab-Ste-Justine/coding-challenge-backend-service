bd49483a1b054cbee8ac7e6c2aa50bf366b3907c
adrian <adipaul1981@yahoo.ca>
2019-08-17 23:12:22 -0400

    Authentication User

---
+++ app/controllers/AuthenticatedUserAction.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import play.api.mvc.Results._
+import play.api.mvc._
+
+/**
+  * https://www.playframework.com/documentation/2.7.x/ScalaActionsComposition#Authentication
+  */
+class AuthenticatedUserAction {
+
+  class LoggingAction @Inject()(parser: BodyParsers.Default)(implicit ec: ExecutionContext)
+    extends ActionBuilderImpl(parser) {
+
+    override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
+      request.session.get(models.Global.SESSION_USER) match {
+        case None =>
+          Future.successful(Forbidden("You are not logged in"))
+        case Some(_) =>
+          block(request)
+      }
+    }
+  }
+
+}
